43ac06e407cbc50d94e7a9ad5aa89455dbd5f806
mutter2009 <32813846+mutter2009@users.noreply.github.com>
2026-08-28 12:58:10 +0800

    Update udpbd.h

--- src/udpbd.h
+++ src/udpbd.h
@@ -17,72 +17,45 @@
 #define UDPBD_MAX_SECTOR_READ  512
 
 /*
- * UDPBD v2 Headers (Fixed for Big-Endian Architectures)
+ * UDPBD v2 Headers
+ * 废弃位域，使用标准 16 位和 32 位整型，防止 MIPS 编译器位域翻转错位
  */
 
 struct SUDPBDv2_Header {
-    union
-    {
-        uint16_t cmd16;
-        struct
-        {
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-            uint16_t cmdpkt : 8; // 0..255
-            uint16_t cmdid  : 3; // 0..8
-            uint16_t cmd    : 5; // 0..31
-#else
-            uint16_t cmd    : 5;
-            uint16_t cmdid  : 3;
-            uint16_t cmdpkt : 8;
-#endif
-        };
-    };	
+    uint16_t cmd16;
 } __attribute__((__packed__));
 
 struct SUDPBDv2_InfoRequest {
-	struct SUDPBDv2_Header hdr;
+    struct SUDPBDv2_Header hdr;
 } __attribute__((__packed__));
 
 struct SUDPBDv2_InfoReply {
-	struct SUDPBDv2_Header hdr;
-	uint32_t sector_size;
-	uint32_t sector_count;
+    struct SUDPBDv2_Header hdr;
+    uint32_t sector_size;
+    uint32_t sector_count;
 } __attribute__((__packed__));
 
 struct SUDPBDv2_RWRequest {
-	struct SUDPBDv2_Header hdr;
-	uint32_t sector_nr;
-	uint16_t sector_count;
+    struct SUDPBDv2_Header hdr;
+    uint32_t sector_nr;
+    uint16_t sector_count;
 } __attribute__((__packed__));
 
 struct SUDPBDv2_WriteDone {
-	struct SUDPBDv2_Header hdr;
-	int32_t result;
+    struct SUDPBDv2_Header hdr;
+    int32_t result;
 } __attribute__((__packed__));
 
-union block_type
-{
+struct SUDPBDv2_BlockType {
     uint32_t bt;
-    struct
-    {
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-        uint32_t spare       : 19;
-        uint32_t block_count :  9;
-        uint32_t block_shift :  4;
-#else
-        uint32_t block_shift :  4;
-        uint32_t block_count :  9;
-        uint32_t spare       : 19;
-#endif
-    };
-};	
+} __attribute__((__packed__));
 
 #define UDP_MAX_PAYLOAD  1472
-#define RDMA_MAX_PAYLOAD (UDP_MAX_PAYLOAD - sizeof(struct SUDPBDv2_Header) - sizeof(union block_type))
+#define RDMA_MAX_PAYLOAD (UDP_MAX_PAYLOAD - sizeof(struct SUDPBDv2_Header) - sizeof(struct SUDPBDv2_BlockType))
 
 struct SUDPBDv2_RDMA {
-	struct SUDPBDv2_Header hdr;
-    union block_type bt;
+    struct SUDPBDv2_Header hdr;
+    struct SUDPBDv2_BlockType bt;
     uint8_t data[RDMA_MAX_PAYLOAD];
 } __attribute__((__packed__));
 
